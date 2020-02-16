9f4311a0dcbb96387374d69febd1891f6ca985b0
kklimexk <klimekkrzys@gmail.com>
2020-02-16 21:51:32 +0100

    handling enum types

--- src/main/scala/utils/ReflectionUtils.scala
+++ src/main/scala/utils/ReflectionUtils.scala
@@ -2,13 +2,20 @@ package utils
 
 import java.util
 
-import scala.reflect._
 import scala.reflect.runtime.universe._
 
 object ReflectionUtils {
 
   def invokeMethod[P, R](obj: Any, arg: Object, methodName: String, paramType: Class[P]): R =
     obj.getClass.getMethod(methodName, paramType).invoke(obj, arg).asInstanceOf[R]
+
+  def getEnumValues[E](enumClass: Class[E]): Array[E] = {
+    val f = enumClass.getDeclaredField("$VALUES")
+    f.setAccessible(true)
+    val o = f.get(null)
+
+    o.asInstanceOf[Array[E]]
+  }
 
   def evalMemberValues[A](topLevelObj: A)(implicit c: TypeTag[A]): A = {
 
@@ -36,39 +43,46 @@ object ReflectionUtils {
             methodFullName.contains("get")) {
             println(s"$methodName, $methodReturnType")
 
-            val returnTypeSymbol = methodReturnType.typeSymbol.asType
+            val returnTypeSymbolFullName = {
+              val typeS = methodReturnType.typeSymbol.asType
+              if (typeS.isJavaEnum)
+                typeS.fullName.split("\\.").mkString("$").replaceFirst("\\$", ".")
+              else typeS.fullName
+            }
 
-            Class.forName(returnTypeSymbol.fullName) match {
-              case t if t == classOf[Object] =>
-                println(t)
-                invokeMethod(obj, null,
-                  methodName.toString.replaceFirst("g", "s"), t)
-              case t if t == classOf[String] =>
-                println(t)
-                invokeMethod(obj, "ExampleString",
-                  methodName.toString.replaceFirst("g", "s"), t)
-              case t if t == classOf[java.lang.Integer] =>
-                println(t)
-                invokeMethod(obj, new java.lang.Integer(30),
-                  methodName.toString.replaceFirst("g", "s"), t)
-              case t if t == classOf[java.lang.Double] =>
-                println(t)
-                invokeMethod(obj, new java.lang.Double(11.1),
-                  methodName.toString.replaceFirst("g", "s"), t)
-              case t if t == classOf[java.lang.Boolean] =>
-                println(t)
-                invokeMethod(obj, new java.lang.Boolean(false),
-                  methodName.toString.replaceFirst("g", "s"), t)
-              case t if t == classOf[java.util.List[_]] =>
-                println(t)
-                invokeMethod(obj, new util.ArrayList(),
-                  methodName.toString.replaceFirst("g", "s"), t)
-              case t =>
-                val newInstance = Class.forName(returnTypeSymbol.fullName).newInstance()
-                invokeMethod(obj, newInstance.asInstanceOf[AnyRef],
-                  methodName.toString.replaceFirst("g", "s"), t)
-                loop(newInstance, methodReturnType)
+            try {
+              Class.forName(returnTypeSymbolFullName) match {
+                case t if t == classOf[Object] =>
+                  invokeMethod(obj, null,
+                    methodName.toString.replaceFirst("g", "s"), t)
+                case t if t == classOf[String] =>
+                  invokeMethod(obj, "ExampleString",
+                    methodName.toString.replaceFirst("g", "s"), t)
+                case t if t == classOf[java.lang.Integer] =>
+                  invokeMethod(obj, new java.lang.Integer(30),
+                    methodName.toString.replaceFirst("g", "s"), t)
+                case t if t == classOf[java.lang.Double] =>
+                  invokeMethod(obj, new java.lang.Double(11.1),
+                    methodName.toString.replaceFirst("g", "s"), t)
+                case t if t == classOf[java.lang.Boolean] =>
+                  invokeMethod(obj, new java.lang.Boolean(false),
+                    methodName.toString.replaceFirst("g", "s"), t)
+                case t if t.isEnum =>
+                  val enumValues = getEnumValues(t)
+                  invokeMethod(obj, enumValues.head.asInstanceOf[AnyRef],
+                    methodName.toString.replaceFirst("g", "s"), t)
+                case t if t == classOf[java.util.List[_]] =>
+                  invokeMethod(obj, new util.ArrayList(),
+                    methodName.toString.replaceFirst("g", "s"), t)
+                case t =>
+                  val newInstance = Class.forName(returnTypeSymbolFullName).newInstance()
+                  invokeMethod(obj, newInstance.asInstanceOf[AnyRef],
+                    methodName.toString.replaceFirst("g", "s"), t)
+                  loop(newInstance, methodReturnType)
                 //loop(mirror.reflect(methodReturnType).instance, methodReturnType)
+              }
+            } catch {
+              case e: ClassNotFoundException => e.printStackTrace()
             }
           }
         }
