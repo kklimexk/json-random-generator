b02f9d3ddaf3bc6b6ea4184362bcf91928d3f9df
kklimexk <>
2020-02-18 17:14:14 +0100

    possibility to specify class name as string

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,19 @@
-import output.ComplexSchema
 import runners.DefaultJsonRandomGeneratorRunner
+import utils.ReflectionUtils._
+
+import scala.reflect.runtime.universe._
 
 object Main {
   def main(args: Array[String]): Unit = {
+    val rootClassName = "output.ComplexSchema"
+
+    val clazz = Class.forName(rootClassName)
+    val mirror = runtimeMirror(clazz.getClassLoader)
+
+    val symbol = mirror.classSymbol(clazz)
+    val typeSignature = symbol.typeSignature
 
     val jsonRandomGeneratorRunner = new DefaultJsonRandomGeneratorRunner()
-    jsonRandomGeneratorRunner.run(new ComplexSchema())
+    jsonRandomGeneratorRunner.run(clazz.newInstance())(type2TypeTag(mirror, typeSignature))
   }
 }
--- src/main/scala/utils/ReflectionUtils.scala
+++ src/main/scala/utils/ReflectionUtils.scala
@@ -1,6 +1,16 @@
 package utils
 
+import scala.reflect.api
+import scala.reflect.runtime.universe._
+
 object ReflectionUtils {
   def invokeMethod[P, R](obj: Any, arg: Object, methodName: String, paramType: Class[P]): R =
     obj.getClass.getMethod(methodName, paramType).invoke(obj, arg).asInstanceOf[R]
+
+  def type2TypeTag[T](mirror: Mirror, tpe: Type): TypeTag[T] =
+    TypeTag(mirror, new api.TypeCreator {
+      def apply[U <: api.Universe with Singleton](m: api.Mirror[U]) =
+        if (m eq mirror) tpe.asInstanceOf[U#Type]
+        else throw new IllegalArgumentException(s"Type tag defined in $mirror cannot be migrated to other mirrors.")
+    })
 }
